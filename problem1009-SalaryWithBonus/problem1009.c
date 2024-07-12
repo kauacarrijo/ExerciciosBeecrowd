157b2a710b7f8629c3aea89c243eae030e5832be
kauacarrijo <kauazagocarrijo@gmail.com>
2024-07-12 19:16:50 -0300

    problem1009

---
+++ problem1009-SalaryWithBonus/problem1009.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char nome[100];
+    double salarioFixo, totalVendas, salarioFinal;
+    scanf("%s", nome);
+    scanf("%lf %lf", &salarioFixo, &totalVendas);
+    salarioFinal = salarioFixo + (totalVendas * 0.15);
+    printf("TOTAL = R$ %.2lf\n", salarioFinal);
+    return 0;
+}
